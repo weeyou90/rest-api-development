2388491fa0b94694fcb7ab15005e2d428bb22b25
weeyou90 <weeyou90@gmail.com>
2018-02-22 05:01:05 -0800

    added some simple and incomplete test commands

---
+++ src/service/testscripts/create_diary.sh
@@ -0,0 +1,9 @@
+echo "Sending invalid post inputs:"
+
+curl localhost:8080/diary/create -X POST -H "Content-Type: application/json" -d '{"token":"2"}'
+
+echo "\n\nSending correct post inputs:"
+
+curl localhost:8080/diary/create -X POST -H "Content-Type: application/json" -d '{"token":"6bf00d02-dffc-4849-a635-a21b08500d61", "title":"No One Can See This Post", "public": false, "text": "It is very secret!"}'
+
+echo "\n"
---
+++ src/service/testscripts/delete_diary.sh
@@ -0,0 +1,9 @@
+echo "Sending invalid post inputs:"
+
+curl localhost:8080/diary/delete -X POST -H "Content-Type: application/json" -d '{"token":"2"}'
+
+echo "\n\nSending correct post inputs:"
+
+curl localhost:8080/diary/delete -X POST -H "Content-Type: application/json" -d '{"token":"6bf00d02-dffc-4849-a635-a21b08500d61", "id": 2}'
+
+echo "\n"
